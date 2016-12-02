448b06b45b3ab9f1ab4edca4093b0d78d5cbf057
mattczyr <mattczyr@gmail.com>
2016-12-01 23:27:27 -0500

    Implement canTeleport check

--- src/main/java/com/chaosthedude/naturescompass/gui/GuiHandler.java
+++ src/main/java/com/chaosthedude/naturescompass/gui/GuiHandler.java
@@ -1,5 +1,6 @@
 package com.chaosthedude.naturescompass.gui;
 
+import com.chaosthedude.naturescompass.items.ItemNaturesCompass;
 import com.chaosthedude.naturescompass.util.BiomeUtils;
 import com.chaosthedude.naturescompass.util.ItemUtils;
 
@@ -10,15 +11,12 @@ import net.minecraftforge.fml.common.network.IGuiHandler;
 
 public class GuiHandler implements IGuiHandler {
 
-	public static final int ID_NATURES_COMPASS = 0;
-
 	@Override
 	public Object getClientGuiElement(int id, EntityPlayer player, World world, int x, int y, int z) {
-		if (id == ID_NATURES_COMPASS) {
-			final ItemStack stack = ItemUtils.getHeldNatureCompass(player);
-			if (stack != null) {
-				return new GuiNaturesCompass(world, player, BiomeUtils.getAllowedBiomes());
-			}
+		final ItemStack stack = ItemUtils.getHeldNatureCompass(player);
+		if (stack != null) {
+			final ItemNaturesCompass natureCompass = (ItemNaturesCompass) stack.getItem();
+			return new GuiNaturesCompass(world, player, natureCompass.canTeleport(player), BiomeUtils.getAllowedBiomes());
 		}
 
 		return null;
