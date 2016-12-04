b96a7aae6e627d322f768d1a387b11afefa75906
mattczyr <mattczyr@gmail.com>
2016-12-03 23:20:10 -0500

    Warn if player does not have permission to teleport

--- src/main/java/com/chaosthedude/naturescompass/network/PacketTeleport.java
+++ src/main/java/com/chaosthedude/naturescompass/network/PacketTeleport.java
@@ -3,9 +3,11 @@ package com.chaosthedude.naturescompass.network;
 import java.util.EnumSet;
 import java.util.Set;
 
+import com.chaosthedude.naturescompass.NaturesCompass;
 import com.chaosthedude.naturescompass.items.ItemNaturesCompass;
 import com.chaosthedude.naturescompass.util.EnumCompassState;
 import com.chaosthedude.naturescompass.util.ItemUtils;
+import com.chaosthedude.naturescompass.util.PlayerUtils;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
@@ -37,22 +39,26 @@ public class PacketTeleport implements IMessage {
 			if (stack != null) {
 				final ItemNaturesCompass natureCompass = (ItemNaturesCompass) stack.getItem();
 				final EntityPlayer player = ctx.getServerHandler().playerEntity;
-				if (natureCompass.canTeleport(player) && natureCompass.getState(stack) == EnumCompassState.FOUND) {
-					final Set<SPacketPlayerPosLook.EnumFlags> set = EnumSet.<SPacketPlayerPosLook.EnumFlags>noneOf(SPacketPlayerPosLook.EnumFlags.class);
-					final int x = natureCompass.getFoundBiomeX(stack);
-					final int z = natureCompass.getFoundBiomeZ(stack);
-					int y = 256;
-					while (player.worldObj.isAirBlock(new BlockPos(x, y - 1, z))) {
-						y--;
-					}
+				if (PlayerUtils.canTeleport(player)) {
+					if (natureCompass.getState(stack) == EnumCompassState.FOUND) {
+						final Set<SPacketPlayerPosLook.EnumFlags> set = EnumSet.<SPacketPlayerPosLook.EnumFlags> noneOf(SPacketPlayerPosLook.EnumFlags.class);
+						final int x = natureCompass.getFoundBiomeX(stack);
+						final int z = natureCompass.getFoundBiomeZ(stack);
+						int y = 256;
+						while (player.worldObj.isAirBlock(new BlockPos(x, y - 1, z))) {
+							y--;
+						}
 
-					player.dismountRidingEntity();
-					((EntityPlayerMP) player).connection.setPlayerLocation(x, y, z, player.cameraYaw, player.cameraPitch, set);
+						player.dismountRidingEntity();
+						((EntityPlayerMP) player).connection.setPlayerLocation(x, y, z, player.cameraYaw, player.cameraPitch, set);
 
-					if (!player.isElytraFlying()) {
-						player.motionY = 0.0D;
-						player.onGround = true;
+						if (!player.isElytraFlying()) {
+							player.motionY = 0.0D;
+							player.onGround = true;
+						}
 					}
+				} else {
+					NaturesCompass.logger.warn("Player " + player.getDisplayNameString() + " tried to teleport but does not have permission.");
 				}
 			}
 
