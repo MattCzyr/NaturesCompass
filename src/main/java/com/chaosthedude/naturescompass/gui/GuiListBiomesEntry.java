355ebe45987839053bf9d6188f56c2bae2ab9789
mattczyr <mattczyr@gmail.com>
2016-12-04 02:01:50 -0500

    Localize precipitation states

--- src/main/java/com/chaosthedude/naturescompass/gui/GuiListBiomesEntry.java
+++ src/main/java/com/chaosthedude/naturescompass/gui/GuiListBiomesEntry.java
@@ -30,11 +30,11 @@ public class GuiListBiomesEntry implements GuiListExtended.IGuiListEntry {
 
 	@Override
 	public void drawEntry(int slotIndex, int x, int y, int listWidth, int slotHeight, Tessellator tessellator, int mouseX, int mouseY, boolean isSelected) {
-		String precipitationState = "None";
+		String precipitationState = I18n.format("string.naturescompass.none");
 		if (biome.getEnableSnow()) {
-			precipitationState = "Snow";
+			precipitationState = I18n.format("string.naturescompass.snow");
 		} else if (biome.rainfall > 0) {
-			precipitationState = "Rain";
+			precipitationState = I18n.format("string.naturescompass.rain");
 		}
 
 		String title = guiNaturesCompass.getSortingCategory().getLocalizedName();
