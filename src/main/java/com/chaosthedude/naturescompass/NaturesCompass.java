6b79dfa6215774ad1fd1748d0e5e6147c84d9fe2
mattczyr <mattczyr@gmail.com>
2016-12-04 01:50:01 -0500

    Refactoring

--- src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
+++ src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
@@ -29,6 +29,7 @@ import net.minecraftforge.oredict.ShapedOreRecipe;
 @Mod(modid = NaturesCompass.MODID, name = NaturesCompass.NAME, version = NaturesCompass.VERSION, acceptedMinecraftVersions = "[1.11]")
 
 public class NaturesCompass {
+
 	public static final String MODID = "naturescompass";
 	public static final String NAME = "Nature's Compass";
 	public static final String VERSION = "1.3.0";
