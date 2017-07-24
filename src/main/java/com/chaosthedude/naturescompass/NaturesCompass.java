9f63df679059f988f9f5c7602bbc96155feb47bd
mattczyr <mattczyr@gmail.com>
2017-07-24 19:40:21 -0400

    Bump version number

--- src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
+++ src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
@@ -28,7 +28,7 @@ public class NaturesCompass {
 
 	public static final String MODID = "naturescompass";
 	public static final String NAME = "Nature's Compass";
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.5.0";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
