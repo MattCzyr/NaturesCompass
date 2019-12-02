b74c9289babf6bb0701349892372e35ba9adc4ed
Matt Czyr <mattczyr@gmail.com>
2019-12-02 00:10:23 -0500

    Bump version number and update changelog

--- src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
+++ src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
@@ -32,7 +32,7 @@ public class NaturesCompass {
 
 	public static final String MODID = "naturescompass";
 	public static final String NAME = "Nature's Compass";
-	public static final String VERSION = "1.6.1";
+	public static final String VERSION = "1.6.2";
 
 	public static final Logger logger = LogManager.getLogger(MODID);
 
