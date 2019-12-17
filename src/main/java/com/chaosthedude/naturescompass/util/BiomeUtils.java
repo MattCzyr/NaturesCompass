c219081942d53c156846d3bac8fa482a4f68a001
NicolasNewman <newma102@gmail.com>
2019-12-16 19:06:25 -0600

    implemented fix for issues with Sponge

--- src/main/java/com/chaosthedude/naturescompass/util/BiomeUtils.java
+++ src/main/java/com/chaosthedude/naturescompass/util/BiomeUtils.java
@@ -70,6 +70,7 @@ public class BiomeUtils {
 				if (!world.isChunkGeneratedAt(x >> 4, z >> 4)) {
 					chunksGenerated++;
 				}
+
 				final Biome biomeAtPos = world.getBiomeForCoordsBody(pos);
 				if (biomeAtPos == biome) {
 					NaturesCompass.logger.info("Search succeeded: " + (int) startPos.getDistance(x, startPos.getY(), z) + " radius, " + samples + " samples, " + chunksGenerated + " chunks generated");
