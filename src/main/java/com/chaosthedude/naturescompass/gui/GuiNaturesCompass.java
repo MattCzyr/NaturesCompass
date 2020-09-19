c562e4e4fd1f029bdadce10c69023984b2b98f1b
various.authors <various.authors@gmail.com>
2020-09-18 22:03:57 -0700

    remove the sort button because it errors

--- src/main/java/com/chaosthedude/naturescompass/gui/GuiNaturesCompass.java
+++ src/main/java/com/chaosthedude/naturescompass/gui/GuiNaturesCompass.java
@@ -40,7 +40,6 @@ public class GuiNaturesCompass extends GuiScreen {
 	private GuiButton teleportButton;
 	private GuiButton infoButton;
 	private GuiButton cancelButton;
-	private GuiButton sortByButton;
 	private GuiButton searchRadiusButton;	
 	private GuiTransparentTextField searchTextField;
 	private GuiListBiomes selectionList;
@@ -93,10 +92,6 @@ public class GuiNaturesCompass extends GuiScreen {
 				teleport();
 			} else if (button == infoButton) {
 				biomesEntry.viewInfo();
-			} else if (button == sortByButton) {
-				sortingCategory = sortingCategory.next();
-				sortByButton.displayString = I18n.format("string.naturescompass.sortBy") + ": " + sortingCategory.getLocalizedName();
-				selectionList.refreshList();
 			} else if (button == searchRadiusButton) {
 			    	searchRadius = searchRadius.next();
 				searchRadiusButton.displayString = I18n.format("string.naturescompass.searchRadius") + ": " + searchRadius.getLocalizedName();
@@ -188,7 +183,6 @@ public class GuiNaturesCompass extends GuiScreen {
 	private void setupButtons() {
 		buttonList.clear();
 		cancelButton = addButton(new GuiTransparentButton(0, 10, height - 30, 110, 20, I18n.format("gui.cancel")));
-		sortByButton = addButton(new GuiTransparentButton(1, 10, 115, 110, 20, I18n.format("string.naturescompass.sortBy") + ": " + sortingCategory.getLocalizedName()));
 		infoButton = addButton(new GuiTransparentButton(2, 10, 90, 110, 20, I18n.format("string.naturescompass.info")));
 		startSearchButton = addButton(new GuiTransparentButton(3, 10, 65, 110, 20, I18n.format("string.naturescompass.startSearch")));
 		teleportButton = addButton(new GuiTransparentButton(4, width - 120, 10, 110, 20, I18n.format("string.naturescompass.teleport")));
