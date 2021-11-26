6bc6bc15a987156ca2972896e78284e71573d046
Matt Czyr <mattczyr@gmail.com>
2021-11-25 21:00:28 -0500

    Add German translations

--- src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
+++ src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
@@ -44,7 +44,7 @@ public class NaturesCompass {
 
 	public static final String MODID = "naturescompass";
 	public static final String NAME = "Nature's Compass";
-	public static final String VERSION = "1.9.0";
+	public static final String VERSION = "1.9.1";
 	
 	public static final String TELEPORT_PERMISSION = "naturescompass.teleport";
 
