33ecb475f99701e99417b473328cf02fd2835121
Jay113355 <foleyjay97@live.com>
2024-04-17 13:16:28 -0500

    Fix not being able to connect to servers that don't have this mod installed.

--- src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
+++ src/main/java/com/chaosthedude/naturescompass/NaturesCompass.java
@@ -82,7 +82,7 @@ public class NaturesCompass {
 	}
 
 	private void preInit(FMLCommonSetupEvent event) {
-		network = ChannelBuilder.named(new ResourceLocation(NaturesCompass.MODID, NaturesCompass.MODID)).networkProtocolVersion(1).optionalServer().clientAcceptedVersions(Channel.VersionTest.exact(1)).simpleChannel();
+		network = ChannelBuilder.named(new ResourceLocation(NaturesCompass.MODID, NaturesCompass.MODID)).networkProtocolVersion(1).optionalClient().clientAcceptedVersions(Channel.VersionTest.exact(1)).simpleChannel();
 
 		// Server packets
 		network.messageBuilder(CompassSearchPacket.class).encoder(CompassSearchPacket::toBytes).decoder(CompassSearchPacket::new).consumerMainThread(CompassSearchPacket::handle).add();
